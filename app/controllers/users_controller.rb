c26e2e9e9516eff922e7e51df336c336ed8096ce
cabelhigh <cabelhigh@yahoo.com>
2017-04-13 16:56:27 -0700

    Playlists now have songs attached to them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,28 @@ require 'rspotify'
 class UsersController < ApplicationController
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @your_playlists = Playlist.where(owner: @spotify_user.uri)
     @spotify_user.playlists.each do |pl|
-      
+      if @your_playlists.select{|y_pls| y_pls.uri==pl.uri}.empty?
+        playlist = Playlist.new
+        playlist.uri = pl.uri
+        playlist.name = pl.name
+        playlist.creator = pl.owner.uri
+        playlist.tracking = true
+        playlist.gen_new_list = true
+        playlist.owner = @spotify_user.uri
+        pl.tracks.each do |t|
+          track = Track.new
+          track.uri = t.uri
+          track.name = t.name
+          playlist.tracks << track
+        end
+        playlist.save
+      else
+
+      end
+      # #Search thru ur pls for the current pl uri, if it exists, don't make a new one
     end
+
   end
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,7 @@
 class Playlist < ApplicationRecord
+  has_many :tracks
+
+  def owner_and_creator?(own, cre)
+    owner==own && creator==cre
+  end
 end
